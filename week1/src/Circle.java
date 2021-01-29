1550e013407ea15a3ffa3ebae7ff10e62c12e532
Kupa <1803862510@qq.com>
2021-01-29 16:42:22 +0800

    题目五

---
+++ week1/src/Circle.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Circle {
+    private static final double PI =3.1415926 ;
+    Scanner in = new Scanner(System.in);
+    private double radius=0;
+    private double area=0;
+    private double perimeter=0;
+    public Circle(double radius){
+        this.radius=radius;
+    }
+    public double getArea(){
+      area=PI*Math.pow(radius,2);
+      return area;
+    }
+    public double getPerimeter(){
+        perimeter=2*PI*radius;
+        return perimeter;
+    }
+public static void main(String[] args){
+        Circle c=new Circle(3);
+        System.out.println(c.getArea());
+        System.out.println(c.getPerimeter());
+}
+}
